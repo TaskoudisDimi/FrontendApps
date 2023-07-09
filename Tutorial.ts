fb8e39c2e3ea3058923f8aabde4427e1ea30ce85
Dimitris Taskoudis <taskoudisdimitris@gmail.com>
2023-07-09 16:31:05 +0300

    Tutorial Angular

--- Tutorial.ts
+++ Tutorial.ts
@@ -1,13 +1,90 @@
+//RUN: 
+// tsc Tutorial.ts && node Tutorial.js
 
 
-function doSomething(){
-    for(var i =0; i < 10; i++){
-        console.log(i);
-    }
-    console.log('Finally value is ' + i);
-}
 
-doSomething();
+//ANOTATIONS
+// let a: number;
+// let b: boolean;
+// let c: string;
+// let d: any;
+// let e: number[] = [1, 2, 3];
+// let f: any[] = [1, true, 'a', false]
+
+// const ColorRed = 0;
+// const ColorGreen = 1;
+// const ColorBlue = 3;
+
+
+// enum Color {Red = 0, Green = 1, Blue = 2}
+// let backgroundColor = Color.Red;
+
+
+// //ASSERTIONS
+// let message;
+// message = 'abc';
+// let endsWithC = (<string>message).endsWith('c');
+// let anotherWatEndsWithC = (message as string).endsWith('c');
+
+
+// //FUNCTIONS
+// let doLog = (message) => {console.log(message)}
+
+
+//Interfaces
+// interface Point{
+//     x: number,
+//     y: number
+// }
+
+// let drawPoint = (point: Point) =>{
+//     //...
+// }
+
+// drawPoint({
+//     x: 1,
+//     y: 2
+// })
+
+
+// //CLASSES
+// class Point{
+//     x: number;
+//     y: number;
+    
+
+//     draw(){
+//         console.log('X: ' + this.x, + 'Y: ' + this.y)
+//     }
+
+//     getDistance(another: Point){
+//         //...
+//     }
+// }
+
+
+
+// let point: Point = new Point();
+// point.x = 1;
+// point.y = 2;
+// point.draw();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

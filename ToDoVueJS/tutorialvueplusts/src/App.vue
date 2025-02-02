764e4200feae988265e33a21aefd9ed5042461d5
DimitrisTaskoudis <taskoudisdimitris@gmail.com>
2025-02-02 21:32:09 +0200

    End of VuePlusTS

--- ToDoVueJS/tutorialvueplusts/src/App.vue
+++ ToDoVueJS/tutorialvueplusts/src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="app">
+    <header>
     <!-- <p> {{ name }} - {{ age }}</p>
     <button @click="changeName('zelda')">Change name</button>
     <button @click="changeAge(30)">Change age</button> -->
     <!-- <p>{{ jobs[0].id }}</p> -->
-  <JobList :jobs="jobs"/>
+      <div class="order">
+        <button @click="handleClick('title')">order by title</button>
+        <button @click="handleClick('salary')">order by salary</button>
+        <button @click="handleClick('location')">order by location</button>
+      </div>
+    </header>
+    <JobList :jobs="jobs" :order="order" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, toRefs, ref } from 'vue';
-import Job from './types/job';
+import { defineComponent, ref } from 'vue';
+import Job from './types/Job';
+import OrderTerm from './types/OrderTerm';
 import JobList from './components/JobsList.vue';
 
 
@@ -39,7 +47,14 @@ export default defineComponent({
       {title: 'Fisherman', location: 'Drama', salary: 90000, id: '4'},  
       {title: 'Prison Guard', location: 'Athina', salary: 80000, id: '5'}, 
     ])
-    return {jobs}
+
+    const order = ref<OrderTerm>('title')
+
+    const handleClick = (term: OrderTerm) => {
+      order.value = term
+    }
+
+    return {jobs, handleClick, order}
   },
   // data(){
   //   return{
@@ -59,5 +74,20 @@ export default defineComponent({
 </script>
 
 <style>
-
+ header {
+    text-align: center;
+  }
+  header .order {
+    margin-top: 20px;
+  }
+  button {
+    margin: 0 10px;
+    color: #1195c9;
+    border: 3px solid #1195c9;
+    background: #d5f0ff;
+    padding: 8px 16px;
+    border-radius: 4px;
+    cursor: pointer;
+    font-weight: bold;
+  }
 </style>
--- ToDoVueJS/tutorialvueplusts/src/components/JobsList.vue
+++ ToDoVueJS/tutorialvueplusts/src/components/JobsList.vue
@@ -1,26 +1,77 @@
 <template>
     <div class="job-list">
+        <p>Ordered by {{ order }}</p>
         <ul>
-            <li v-for="job in jobs" :key="job.id"></li>
+            <li v-for="job in orderedJobs" :key="job.id">
+                <h2>{{ job.title }} in {{ job.location }}</h2>
+                <div class="library">
+                    <p>{{ job.salary }} rupees</p>
+                </div>
+                <div class="description">
+                    <p>Test Test Test Test</p>
+                </div>
+            </li>
         </ul>
     </div>
 </template>
 
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
-import Job from '@/types/job'
+import { computed, defineComponent, PropType } from 'vue'
+import Job from '/media/dimitristaskoudis/HardDisk1/Codes/FrontendApps/ToDoVueJS/tutorialvueplusts/src/types/Job'
+import OrderTerm from '/media/dimitristaskoudis/HardDisk1/Codes/FrontendApps/ToDoVueJS/tutorialvueplusts/src/types/OrderTerm'
+
 
 export default defineComponent({
     props: {
         jobs: {
             required: true,
             type: Array as PropType<Job[]>
+        },
+        order: {
+            required: true,
+            type: String as PropType<OrderTerm>
         }
+    },
+    setup(props){
+        const orderedJobs = computed(() => {
+            return [...props.jobs].sort((a: Job, b: Job) => {
+                return a[props.order] > b[props.order] ? 1 : -1
+            }) 
+        })
+        return {orderedJobs}
     }
 })
 </script>
 
 <style>
-
+.job-list {
+    max-width: 960px;
+    margin: 40px auto;
+  }
+  .job-list ul {
+    padding: 0;
+  }
+  .job-list li {
+    list-style-type: none;
+    background: white;
+    padding: 16px;
+    margin: 16px 0;
+    border-radius: 4px;
+  }
+  .job-list h2 {
+    margin: 0 0 10px;
+    text-transform: capitalize;
+  }
+  .salary {
+    display: flex;
+  }
+  .salary img {
+    width: 30px;
+  }
+  .salary p {
+    color: #17bf66;
+    font-weight: bold;
+    margin: 10px 4px;
+  }
 </style>

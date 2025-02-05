d525b9f4714c4ddea87a711f4224a078505fdbbb
DimitrisTaskoudis <taskoudisdimitris@gmail.com>
2025-02-05 22:13:27 +0200

    Insert images and start working on todoapp

---
+++ ToDoFlutter/todoapp/lib/screen/login.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LogIN_Screen extends StatefulWidget {
+  const LogIN_Screen({super.key});
+
+  @override
+  State<LogIN_Screen> createState() => _LogIN_ScreenState();
+}
+
+class _LogIN_ScreenState extends State<LogIN_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
